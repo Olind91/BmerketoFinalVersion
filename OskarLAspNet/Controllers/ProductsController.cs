1e807d1435bbbd4ebe1fc1e2111e870bf635b52c
oskar <Osklind91@hotmail.com>
2023-05-17 19:39:57 +0200

    Add project files.

---
+++ OskarLAspNet/Controllers/ProductsController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OskarLAspNet.Helpers.Services;
+using OskarLAspNet.Models.Dtos;
+using OskarLAspNet.Models.ViewModels;
+
+namespace OskarLAspNet.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductService _productService;
+        private readonly TagService _tagService;
+
+        public ProductsController(ProductService productService, TagService tagService)
+        {
+            _productService = productService;
+            _tagService = tagService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllAsync();
+            return View(products);
+        }
+
+
+
+
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Tags = await _tagService.GetTagsAsync();
+
+
+
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Create(ProductRegVM viewModel, string[] tags)
+        {
+            if (ModelState.IsValid)
+            {
+                var product = await _productService.CreateAsync(viewModel);
+                if (product != null)
+                {
+                    if (viewModel.Image != null)
+                        await _productService.UploadImageAsync(product, viewModel.Image);
+
+                    await _productService.AddProductTagsAsync(viewModel, tags);
+
+                    return RedirectToAction("Create");
+                }
+
+                ModelState.AddModelError("", "Something Went Wrong.");
+            }
+
+            ViewBag.Tags = await _tagService.GetTagsAsync();
+            return View(viewModel);
+        }
+
+
+
+
+        public async Task<IActionResult> ProductDetails(string articleNumber)
+        {
+            var product = await _productService.GetByArticleNumberAsync(articleNumber);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new ProductDetailViewModel
+            {
+                ArticleNumber = product.ArticleNumber,
+                Name = product.ProductName,
+                Price = product.Price,
+                Description = product.ProductDescription,
+                ImageUrl = product.ImageUrl
+            };
+
+            return View(viewModel);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
