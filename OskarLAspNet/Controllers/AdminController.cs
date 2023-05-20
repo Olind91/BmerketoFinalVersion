e25f544f13b1f15dacc03700f6e89d183cf42abe
oskar <Osklind91@hotmail.com>
2023-05-20 12:03:15 +0200

    account styling

--- OskarLAspNet/Controllers/AdminController.cs
+++ OskarLAspNet/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using OskarLAspNet.Contexts;
 using OskarLAspNet.Helpers.Services;
@@ -13,12 +14,14 @@ namespace OskarLAspNet.Controllers
         private readonly DataContext _dataContext;
         private readonly ProductService _productService;
         private readonly ContactFormService _contactFormService;
+        private readonly UserManager<AppUser> _userManager;
 
-        public AdminController(DataContext dataContext, ProductService productService, ContactFormService contactFormService)
+        public AdminController(DataContext dataContext, ProductService productService, ContactFormService contactFormService, UserManager<AppUser> userManager)
         {
             _dataContext = dataContext;
             _productService = productService;
             _contactFormService = contactFormService;
+            _userManager = userManager;
         }
 
         [Authorize(Roles = "admin")]
@@ -52,6 +55,30 @@ namespace OskarLAspNet.Controllers
             var comments = await _contactFormService.GetAllAsync();
             return View(comments);
 
+        }
+
+        //TEST
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> ChangeUserRole(string userId, string newRole)
+        {
+            // Retrieve the user based on the provided userId
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                // User not found, handle accordingly
+                return RedirectToAction("GetAllUsers");
+            }
+
+            // Retrieve the current roles of the user
+            var currentRoles = await _userManager.GetRolesAsync(user);
+
+            // Remove the current roles
+            await _userManager.RemoveFromRolesAsync(user, currentRoles);
+
+            // Assign the new role
+            await _userManager.AddToRoleAsync(user, newRole);
+
+            return RedirectToAction("GetAllUsers");
         }
 
     }
